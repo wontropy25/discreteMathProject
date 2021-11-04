7f767572e421d331e7a8f497aa89589779574ab6
Jaewon <yjwon20@naver.com>
2021-11-04 13:58:49 +0900

    초기 커밋.

---
+++ main.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <string>
+#include <cstdio>
+#include <vector>
+
+using namespace std;
+
+int main() {
+
+	/* 1. 파일 입출력 처리 */
+
+	string fileName1 = "input.txt";
+	string fileName2 = "input2.txt";
+
+
+	/* 2. 1번 과제 (관계 행렬) */
+	vector<vector<int>> graph1;
+
+
+	/* 3. 2번 과제 (가중치 행렬) */
+	vector<vector<int>> graph2;
+
+}
+
