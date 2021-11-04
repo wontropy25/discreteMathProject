c1967dd3621c0f1ac1e1b1ded0b07e25789de642
Jaewon Yoon <76518945+yjwon20@users.noreply.github.com>
2021-11-04 11:21:40 +0900

    Add files via upload

---
+++ 이산수학 7팀 라이브쉐어 테스트/DMteamProject.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <cstdio>
+#include <vector>
+
+using namespace std;
+
+int main() {
+	// 들어오시면 각자 이름 작성해주세요
+
+	// 윤재원
+	// 조재광
+	// 김남진
+	// 이윤정
+	
+	cout << "Hello World!" << endl;
+
+	cout << "안녕하세요!";
+}
+
